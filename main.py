26c21a8d6047524c4505ea909db52e3b7f448275
clout <59515681+cloutjs@users.noreply.github.com>
2022-03-14 17:27:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import requests
 url = input("URL: ")
 r = requests.get(url)
 try:
-  vidid = r.url.split("video/")[1].split("?")[0]
+    vidid = r.url.split("video/")[1].split("?")[0]
 except:
-  try:
-    vidid = r.url.split("https://m.tiktok.com/v/")[1].split(".html")[0]
-  except:
-    print("An unknown error occured.")
-
+    try:
+        vidid = r.url.split("https://m.tiktok.com/v/")[1].split(".html")[0]
+    except:
+        print("An unknown error occured.")
 print(vidid)
